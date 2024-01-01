fb0a37b0af0179881c95ad5578db6b2c69dd048c
tengkurizki13 <trizkidinelvipratama@gmail.com>
2024-01-01 20:33:06 +0700

    feat: add socker.io

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const env = require("dotenv").config();
 const express = require("express");
 const router = require("./routes");
 const app = express();
+
+const http = require("http")
+const server = http.createServer(app)
+const { Server } = require("socket.io");
+const io = new Server(server)
+
 const port = process.env.PORT || 3030;
 const cors = require("cors");
 
@@ -10,7 +16,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-app.listen(port, () => {
+
+io.on("connection",(socket) => {
+  socket.on("message",(data) => {
+    const {id,message} = data
+    socket.broadcast.emit("message",id,message)
+  })
+})
+
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
   
